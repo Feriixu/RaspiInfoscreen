efa68fc870249b1f63c3abd26fe37f5ae5f34e8c
Feriixu <39712181+Feriixu@users.noreply.github.com>
2019-07-02 16:53:10 +0200

    Create kiosk.sh

---
+++ kiosk.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+sitePresentation=""
+sitePersonio=""
+
+# Disable screen timeout
+xset s noblank
+xset s off
+xset -dpms
+
+# Hide cursor if idle for 0.5 sceconds
+unclutter -idle 0.5 -root &
+
+# Clear chromium flags to disable warning banner
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+
+# Wait for secure startup
+sleep 10
+
+# Launch chromium
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --no-sandbox --start-fullscreen $sitePresentation $sitePersonio &
+
+# Wait for browser to start up
+sleep 20;
+
+# move mouse to corner to prevent pop ups on website
+xdotool mousemove 0 0
+
+# Loop
+while true; do
+        #Show presentation
+        sleep 25;
+
+        #Change to personio
+        xdotool key ctrl+Tab;
+        #Reload Personio
+        xdotool key F5;
+        #Wait for website
+        sleep 4;
+        #Move down
+        for n in {1..20}; do
+                xdotool key Down;
+                sleep 2;
+done
+#Wait a little
+sleep 20;
+
+# move mouse to corner to prevent pop ups on website
+xdotool mousemove 0 0
+
+# Loop
+while true; do
+        #Show presentation
+        sleep 25;
+
+        #Change to personio
+        xdotool key ctrl+Tab;
+        #Reload Personio
+        xdotool key F5;
+        #Wait for website
+        sleep 4;
+        #Move down
+        for n in {1..20}; do
+                xdotool key Down;
+                sleep 2;
+        done
+        #Wait a little
+        sleep 5
+
+        #Change back to presentation
+        xdotool key ctrl+Tab;
+        #Reload presentation
+        xdotool key F5;
+done
